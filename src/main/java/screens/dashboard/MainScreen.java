5814dad7a4e9ed7479d04c057ecd2d32cf9f8d19
Deepakv <dipak.kumar1225@gmail.com>
2021-10-07 08:40:44 +0530

    Added [1] new Element locator mobileElementElvChapter1Content [2] new condition isChapter1ContentDisplayed()

--- src/main/java/screens/dashboard/MainScreen.java
+++ src/main/java/screens/dashboard/MainScreen.java
@@ -3,7 +3,11 @@ package screens.dashboard;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.pagefactory.AndroidFindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import screens.BaseScreen;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class MainScreen extends BaseScreen {
 
@@ -14,6 +18,9 @@ public class MainScreen extends BaseScreen {
     @AndroidFindBy(xpath = "//android.widget.TextView[contains(@text,'Chapter 1: App Fundamentals')]")
     private MobileElement mobileElementElvChapter1;
 
+    @AndroidFindBy(xpath = "//android.widget.TextView[contains(@text,'Basic TextView')]")
+    private List<MobileElement> mobileElementElvChapter1Content;
+
     @AndroidFindBy(xpath = "//android.widget.TextView[contains(@text,'Chapter 2: User Interface')]")
     private MobileElement mobileElementElvChapter2;
 
@@ -21,7 +28,12 @@ public class MainScreen extends BaseScreen {
         mobileElementElvChapter1.click();
     }
 
+    public boolean isChapter1ContentDisplayed() {
+        getBaseMobileDriver().manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+        return mobileElementElvChapter1Content.size() > 0;
+    }
+
     public void selectChapter2() {
-        mobileElementElvChapter2.click();
+        getWebDriverWait().until(ExpectedConditions.refreshed(ExpectedConditions.elementToBeClickable(mobileElementElvChapter2))).click();
     }
 }
