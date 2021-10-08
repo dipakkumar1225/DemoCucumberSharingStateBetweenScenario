abfd13c5b3eb25ffdf665a6a0f834054a1e992ed
Deepakv <dipak.kumar1225@gmail.com>
2021-10-08 08:55:14 +0530

    Changes in BaseScreen.java -> Removed duration provided in AppiumFieldDecorator

--- src/main/java/screens/BaseScreen.java
+++ src/main/java/screens/BaseScreen.java
@@ -6,7 +6,6 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import static java.time.Duration.ofSeconds;
 
 public class BaseScreen {
 
@@ -18,8 +17,7 @@ public class BaseScreen {
 
     protected BaseScreen(AppiumDriver<MobileElement> appiumDriver) {
         this.baseMobileDriver = appiumDriver;
-        AppiumFieldDecorator appiumFieldDecorator = new AppiumFieldDecorator(this.baseMobileDriver, ofSeconds(60));
-        PageFactory.initElements(appiumFieldDecorator, this);
-        webDriverWait = new WebDriverWait(getBaseMobileDriver(), 20);
+        PageFactory.initElements(new AppiumFieldDecorator(this.baseMobileDriver), this);
+        webDriverWait = new WebDriverWait(getBaseMobileDriver(), 30);
     }
 }
